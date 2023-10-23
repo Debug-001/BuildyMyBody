9cf7b13d2a7a6c60a31fbf7ca43a0063d541a3d0
Debug-001 <ombhatt1158@gmail.com>
2023-10-23 14:59:05 +0530

    images path reworked assigned

--- dist/client/build/_shared/chunk-AZ5RRHEV.js
+++
@@ -1,7 +0,0 @@
-// dist/client/img/first.jpeg
-var first_default = "/build/_assets/first-UFO2HWLU.jpeg";
-
-export {
-  first_default
-};
-//# sourceMappingURL=/build/_shared/chunk-AZ5RRHEV.js.map
--- dist/client/build/routes/data-4FJDT2YF.js
+++
@@ -1,9 +0,0 @@
-import {
-  data_default
-} from "/build/_shared/chunk-QQR23WLR.js";
-import "/build/_shared/chunk-AZ5RRHEV.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export {
-  data_default as default
-};
-//# sourceMappingURL=/build/routes/data-4FJDT2YF.js.map
