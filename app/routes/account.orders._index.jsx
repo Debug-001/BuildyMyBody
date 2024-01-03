dc71a482e8858af152654e3c4d606fffe4298f9e
randeep007 <randeepkabir@gmail.com>
2024-01-04 02:07:34 +0530

    4th jan

--- app/routes/account.orders._index.jsx
+++ app/routes/account.orders._index.jsx
@@ -1,13 +1,13 @@
-import {Link, NavLink, useLoaderData} from '@remix-run/react';
-import {Money, Pagination, getPaginationVariables} from '@shopify/hydrogen';
-import {json, redirect} from '@shopify/remix-oxygen';
+import { Link, NavLink, useLoaderData } from '@remix-run/react';
+import { Money, Pagination, getPaginationVariables } from '@shopify/hydrogen';
+import { json, redirect } from '@shopify/remix-oxygen';
 
 export const meta = () => {
-  return [{title: 'BuildMyBody|Orders'}];
+  return [{ title: 'BuildMyBody|Orders' }];
 };
 
-export async function loader({request, context}) {
-  const {session, storefront} = context;
+export async function loader({ request, context }) {
+  const { session, storefront } = context;
 
   const customerAccessToken = await session.get('customerAccessToken');
   if (!customerAccessToken?.accessToken) {
@@ -19,7 +19,7 @@ export async function loader({request, context}) {
       pageBy: 20,
     });
 
-    const {customer} = await storefront.query(CUSTOMER_ORDERS_QUERY, {
+    const { customer } = await storefront.query(CUSTOMER_ORDERS_QUERY, {
       variables: {
         customerAccessToken: customerAccessToken.accessToken,
         country: storefront.i18n.country,
@@ -33,18 +33,18 @@ export async function loader({request, context}) {
       throw new Error('Customer not found');
     }
 
-    return json({customer});
+    return json({ customer });
   } catch (error) {
     if (error instanceof Error) {
-      return json({error: error.message}, {status: 400});
+      return json({ error: error.message }, { status: 400 });
     }
-    return json({error}, {status: 400});
+    return json({ error }, { status: 400 });
   }
 }
 
 export default function Orders() {
-  const {customer} = useLoaderData();
-  const {orders, numberOfOrders} = customer;
+  const { customer } = useLoaderData();
+  const { orders, numberOfOrders } = customer;
   return (
     <>
       <div className="container">
@@ -69,13 +69,13 @@ export default function Orders() {
   );
 }
 
-function OrdersTable({orders}) {
+function OrdersTable({ orders }) {
   return (
     <>
       <div className="acccount-orders">
         {orders?.nodes.length ? (
           <Pagination connection={orders}>
-            {({nodes, isLoading, PreviousLink, NextLink}) => {
+            {({ nodes, isLoading, PreviousLink, NextLink }) => {
               return (
                 <>
                   <PreviousLink>
@@ -113,63 +113,60 @@ function EmptyOrders() {
   );
 }
 
-function OrderItem({order}) {
+function OrderItem({ order }) {
   return (
     <>
       <fieldset>
         <div className="row">
-          <div className="col-5 col-lg-6 col-md-6">
-            {/* <img src={order.lineItems.nodes[0].variant[0].image.url} alt="" /> */}
-          </div>
-          <div className="col-7 col-md-6 col-lg-6">
+
+          <div className="col-12 ">
             <div className="row">
-              <div className="col-12 col-lg-6 col-md-6 ">
+              <div className="col-12 col-lg-3 col-md-6 ">
                 <div className="">
-                  <h5 style={{fontWeight: 'bold'}}>Order Id</h5>
+                  <h5 style={{ fontWeight: 'bold' }}>Order Id</h5>
                   <Link to={`/account/orders/${order.id}`}>
-                    <span style={{fontSize: '1.2rem', color: 'black'}}>
+                    <span style={{ fontSize: '1.2rem', color: 'black' }}>
                       #{order.orderNumber}
                     </span>
                   </Link>
                 </div>
               </div>
-              <div className="col-12 col-lg-6 col-md-6">
+              <div className="col-12 col-lg-3 col-md-6">
                 <div className="mt-3 mt-lg-0 mt-md-0">
-                  <h5 style={{fontWeight: 'bold'}}>Date</h5>
-                  <p style={{fontSize: '1.2rem'}}>
+                  <h5 style={{ fontWeight: 'bold' }}>Date</h5>
+                  <p style={{ fontSize: '1.2rem' }}>
                     {new Date(order.processedAt).toDateString()}
                   </p>
                 </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col-12 col-lg-6 col-md-6">
-                <div className="mt-3">
-                  <h5 style={{fontWeight: 'bold'}}>Payment Status</h5>
-                  <p style={{fontSize: '1.2rem'}}>{order.financialStatus}</p>
+              <div className="col-12 col-lg-3 col-md-6">
+                <div className="mt-3 mt-lg-0 mt-md-0">
+                  <h5 style={{ fontWeight: 'bold' }}>Payment Status</h5>
+                  <p style={{ fontSize: '1.2rem' }}>{order.financialStatus}</p>
                 </div>
               </div>
-              <div className="col-12 col-lg-6 col-md-6">
-                <div className="mt-3">
-                  <h5 style={{fontWeight: 'bold'}}>Fulfillment Status</h5>
-                  <p style={{fontSize: '1.2rem'}}>{order.fulfillmentStatus}</p>
+              <div className="col-12 col-lg-3 col-md-6">
+                <div className="mt-3 mt-lg-0 mt-md-0">
+                  <h5 style={{ fontWeight: 'bold' }}>Fulfillment Status</h5>
+                  <p style={{ fontSize: '1.2rem' }}>{order.fulfillmentStatus}</p>
                 </div>
               </div>
             </div>
+
           </div>
         </div>
         <div className="row mt-5">
-          <div className="col-lg-6 col-md-6 col-sm-12 d-flex justify-content-lg-center ">
-            <h5 style={{fontWeight: 'bold'}}>Total</h5>
-            <span style={{fontSize: '1.2rem'}} className="ml-4">
+          <div className="col-lg-3 col-md-6 col-sm-12 d-flex justify-content-lg-center ">
+            <h5 style={{ fontWeight: 'bold' }}>Total</h5>
+            <span style={{ fontSize: '1.2rem' }} className="ml-4">
               <Money data={order.currentTotalPrice} />
             </span>
           </div>
-          <div className="col-lg-6 col-md-6 col-sm-12  d-flex justify-content-lg-center mt-3 mt-lg-0 mt-md-0">
+          <div className="col-lg-3 col-md-6 col-sm-12  d-flex justify-content-lg-center mt-3 mt-lg-0 mt-md-0">
             <Link to={`/account/orders/${btoa(order.id)}`}>
               <div
                 className="btn shop-nav-btn p-2"
-                style={{fontSize: '1.2rem'}}
+                style={{ fontSize: '1.2rem' }}
               >
                 View Order
               </div>
