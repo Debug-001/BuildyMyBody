caf373729f28789961bbbf2a5b4c6457f7091d6a
Debug-001 <ombhatt1158@gmail.com>
2024-01-04 20:50:15 +0530

    api route not found

--- app/Components/Home/Verify.jsx
+++ app/Components/Home/Verify.jsx
@@ -1,32 +1,60 @@
-import React from 'react';
-import article1 from '../../img/article-1.png';
+import {useState} from 'react';
+// import article1 from '../../img/article-1.png';
 
 const Verify = () => {
+  const [code, setCode] = useState('');
+  const [message, setMessage] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({code}),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setMessage(data.message);
+      } else {
+        setMessage('Code not found.');
+      }
+    } catch (error) {
+      console.error('Error verifying code:', error);
+      setMessage('Error verifying code.');
+    }
+  };
+
   return (
     <>
       <section id="verify">
         <div className="container pt-5 pb-5">
           <div
             className="d-flex justify-content-center"
-            style={{ flexDirection: 'column', alignItems: 'center' }}
+            style={{flexDirection: 'column', alignItems: 'center'}}
           >
             <h1
               className="custom-heading3 text-center"
-              style={{ color: '#ff2828', fontWeight: '900' }}
+              style={{color: '#ff2828', fontWeight: '900'}}
             >
               <em>Verify Your Products</em>
             </h1>
             <hr className="h1-hr" />
           </div>
-
-          {/* <h1 className='d-flex justify-content-center font-weight-bold custom-heading2'><em>VERIFY YOUR PRODUCT</em></h1> */}
-          <div className="verify-img d-flex justify-content-center">
-            <img
-              className="verify-vid mt-5 "
-              style={{ width: '100%' }}
-              src={article1}
-              alt="verify-product-vid"
-            />
+          <div>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+                placeholder="Enter 14-digit code"
+              />
+              <button type="submit">Verify</button>
+            </form>
+            {message && <p>{message}</p>}
           </div>
         </div>
       </section>
