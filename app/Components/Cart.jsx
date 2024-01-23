a27a7279619b301b6ff5c7c09817094b7728e66f
Debug-001 <ombhatt1158@gmail.com>
2024-01-23 23:28:13 +0530

    removed unsuported new layout, old 4.6 bootstrap version

--- app/Components/Cart.jsx
+++ app/Components/Cart.jsx
@@ -1,18 +1,16 @@
-import { Link} from '@remix-run/react';
-import { CartForm } from '@shopify/hydrogen';
-import { flattenConnection, Image, Money } from '@shopify/hydrogen-react';
-import { FaPlus } from 'react-icons/fa6';
-import { FaMinus } from 'react-icons/fa6';
+import {Link} from '@remix-run/react';
+import {CartForm} from '@shopify/hydrogen';
+import {flattenConnection, Image, Money} from '@shopify/hydrogen-react';
+import {FaPlus} from 'react-icons/fa6';
+import {FaMinus} from 'react-icons/fa6';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-
 export const meta = () => {
-  return [{ title: `BuildMyBody|Cart` }];
+  return [{title: `BuildMyBody|Cart`}];
 };
 
-
-export function CartLineItems({ linesObj }) {
+export function CartLineItems({linesObj}) {
   const lines = flattenConnection(linesObj);
   return (
     <div className="space-y-8">
@@ -45,17 +43,17 @@ const settings = {
   ],
 };
 
-function ItemRemoveButton({ lineIds }) {
+function ItemRemoveButton({lineIds}) {
   return (
     <CartForm
       route="/cart"
       action={CartForm.ACTIONS.LinesRemove}
-      inputs={{ lineIds }}
+      inputs={{lineIds}}
     >
       <button
         id="remove-product"
         className="text-dark mt-4"
-        style={{ background: 'white' }}
+        style={{background: 'white'}}
         type="submit"
       >
         <p>Remove</p>
@@ -64,8 +62,8 @@ function ItemRemoveButton({ lineIds }) {
   );
 }
 
-function LineItem({ lineItem }) {
-  const { merchandise, quantity } = lineItem;
+function LineItem({lineItem}) {
+  const {merchandise, quantity} = lineItem;
   return (
     <>
       <div className="all-info  mt-5 row">
@@ -100,7 +98,7 @@ function LineItem({ lineItem }) {
           <div className="d-flex justify-content-left justify-content-lg-center justify-content-md-center ">
             <div
               className="d-flex p-2 mt-4 flex-row-reverse"
-              style={{ border: '1px solid black', borderRadius: '4px' }}
+              style={{border: '1px solid black', borderRadius: '4px'}}
             >
               <div>
                 <CartForm
@@ -117,7 +115,7 @@ function LineItem({ lineItem }) {
                   }}
                 >
                   <button
-                    style={{ border: 'none', background: 'none' }}
+                    style={{border: 'none', background: 'none'}}
                     className="ml-4"
                   >
                     {' '}
@@ -144,7 +142,7 @@ function LineItem({ lineItem }) {
                   }}
                 >
                   <button
-                    style={{ border: 'none', background: 'none' }}
+                    style={{border: 'none', background: 'none'}}
                     className="mr-4"
                   >
                     {' '}
@@ -166,12 +164,12 @@ function LineItem({ lineItem }) {
     </>
   );
 }
-export function CartSummary({ cost, checkoutUrl, collection }) {
+export function CartSummary({cost, checkoutUrl, collection}) {
   if (!checkoutUrl) return null;
   return (
     <>
       <div className="card-1 px-4 py-2 mt-2">
-        <p className="text-start mt-2" id="order">
+        <p className="text-center mt-2" id="order">
           My Order Summary
         </p>
         <div
@@ -197,11 +195,111 @@ export function CartSummary({ cost, checkoutUrl, collection }) {
           )}
         </div>
         <hr className="cart-hr bg-dark w-100 mt-3 " />
-        <Link to={checkoutUrl} className="btn w-100 checkout-button" target="_blank" rel=" noopener noreferrer ">
+        <Link
+          to={checkoutUrl}
+          className="btn w-100 checkout-button"
+          target="_blank"
+          rel=" noopener noreferrer "
+        >
           Checkout
         </Link>
+        <hr className="cart-hr bg-dark w-100 mt-3 " />
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header">
+              <button
+                className="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                Accordion Item #1
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              className="accordion-collapse collapse show"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                <strong>This is the first item's accordion body.</strong> It is
+                shown by default, until the collapse plugin adds the appropriate
+                classes that we use to style each element. These classes control
+                the overall appearance, as well as the showing and hiding via
+                CSS transitions. You can modify any of this with custom CSS or
+                overriding our default variables. It's also worth noting that
+                just about any HTML can go within the{' '}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                Accordion Item #2
+              </button>
+            </h2>
+            <div
+              id="collapseTwo"
+              className="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                <strong>This is the second item's accordion body.</strong> It is
+                hidden by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the{' '}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
+              >
+                Accordion Item #3
+              </button>
+            </h2>
+            <div
+              id="collapseThree"
+              className="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                <strong>This is the third item's accordion body.</strong> It is
+                hidden by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the{' '}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
     </>
   );
 }
