34ef60c713e86fc70e08d7f857e864d8f8d57111
Randeep Singh <randeep.x.singh@goindigo.in>
2024-04-21 17:04:44 +0530

    removed review

--- app/components/Product/ProductConstants.jsx
+++ app/components/Product/ProductConstants.jsx
@@ -11,11 +11,11 @@ export const ProductConstants = {
             href: 'profile',
             label: 'Directions To Use',
         },
-        {
-            id: 'reviews',
-            href: 'contact',
-            label: 'Reviews',
-        },
+        // {
+        //     id: 'reviews',
+        //     href: 'contact',
+        //     label: 'Reviews',
+        // },
     ],
     faqData: [
         {
@@ -77,17 +77,17 @@ export const ProductConstants = {
         },
     ],
 
-    ReviewText: `Etsy mixtape wayfarers, ethical wes anderson tofu before
-    they sold out mcsweeney's organic lomo retro fanny pack
-    lo-fi farm-to-table readymade. Messenger bag gentrify
-    pitchfork tattooed craft beer, iphone skateboard locavore
-    carles etsy salvia banksy hoodie helvetica. DIY synth PBR
-    banksy irony. Leggings gentrify squid 8-bit cred
-    pitchfork. Williamsburg banh mi whatever gluten-free,
-    carles pitchfork biodiesel fixie etsy retro mlkshk vice
-    blog. Scenester cred you probably haven't heard of them,
-    vinyl craft beer blog stumptown. Pitchfork sustainable
-    tofu synth chambray yr.`
+    // ReviewText: `Etsy mixtape wayfarers, ethical wes anderson tofu before
+    // they sold out mcsweeney's organic lomo retro fanny pack
+    // lo-fi farm-to-table readymade. Messenger bag gentrify
+    // pitchfork tattooed craft beer, iphone skateboard locavore
+    // carles etsy salvia banksy hoodie helvetica. DIY synth PBR
+    // banksy irony. Leggings gentrify squid 8-bit cred
+    // pitchfork. Williamsburg banh mi whatever gluten-free,
+    // carles pitchfork biodiesel fixie etsy retro mlkshk vice
+    // blog. Scenester cred you probably haven't heard of them,
+    // vinyl craft beer blog stumptown. Pitchfork sustainable
+    // tofu synth chambray yr.`
 
 }
 
--- app/components/Product/index.jsx
+++ app/components/Product/index.jsx
@@ -1,18 +1,18 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Navbar from '../Navbar';
 import Footer from '../Footer/Footer';
-import {FaPlus} from 'react-icons/fa6';
-import {FaMinus} from 'react-icons/fa6';
-import {ShopPayButton} from '@shopify/hydrogen-react';
-import {Image, Money} from '@shopify/hydrogen';
+import { FaPlus } from 'react-icons/fa6';
+import { FaMinus } from 'react-icons/fa6';
+import { ShopPayButton } from '@shopify/hydrogen-react';
+import { Image, Money } from '@shopify/hydrogen';
 import ProductCarousal from './ProductCarousal';
 import ProductOptions from './ProductOptions';
 import ProductForm from './ProductForm';
-import {Link} from '@remix-run/react';
+import { Link } from '@remix-run/react';
 import discountsvg from '../../img/discountsvg.png';
 import ProductConstants from './ProductConstants';
 
-const Product = ({data}) => {
+const Product = ({ data }) => {
   const {
     product,
     selectedVariant,
@@ -87,11 +87,11 @@ const Product = ({data}) => {
               {/* middle column  */}
               <div
                 className="col-sm-12 col-lg-6 col-md-12 mt-5 "
-                style={{padding: '0rem 1rem'}}
+                style={{ padding: '0rem 1rem' }}
               >
                 <h2
                   className="text-lg-left text-md-center text-sm-center product-title-size"
-                  style={{fontWeight: '800'}}
+                  style={{ fontWeight: '800' }}
                 >
                   {product.title}
                 </h2>
@@ -100,7 +100,7 @@ const Product = ({data}) => {
                   <div className="col-lg-6 col-md-6 col-12 flex-column">
                     <div>
                       {selectedVariant.compareAtPrice && (
-                        <div className="d-flex" style={{fontSize: '25px'}}>
+                        <div className="d-flex" style={{ fontSize: '25px' }}>
                           <p className="title  ">MRP:</p>
                           <span>
                             <Money
@@ -108,7 +108,7 @@ const Product = ({data}) => {
                               data={selectedVariant.compareAtPrice}
                               className="ml-2"
                               as="del"
-                              style={{color: '#ff2828'}}
+                              style={{ color: '#ff2828' }}
                             />
                           </span>
                         </div>
@@ -120,21 +120,21 @@ const Product = ({data}) => {
                             withoutTrailingZeros
                             data={selectedVariant.price}
                             className="ml-2"
-                            style={{fontSize: '38px'}}
+                            style={{ fontSize: '38px' }}
                           />
                         </div>
                         {selectedVariant.compareAtPrice && (
                           <div>
                             <div className="ml-1 mt-1 d-flex align-items-center ">
                               <p
-                                style={{color: '#ff2828'}}
+                                style={{ color: '#ff2828' }}
                                 className="product-btn p-2"
                               >
                                 {Math.ceil(
                                   ((selectedVariant.compareAtPrice.amount -
                                     selectedVariant.price.amount) /
                                     selectedVariant.compareAtPrice.amount) *
-                                    100,
+                                  100,
                                 )}
                                 % off
                               </p>
@@ -146,7 +146,7 @@ const Product = ({data}) => {
                     <div className="d-flex width-input mt-3 border border-dark justify-content-between p-1">
                       <div>
                         <button
-                          style={{border: 'none', background: 'none'}}
+                          style={{ border: 'none', background: 'none' }}
                           className="ml-4"
                           onClick={() => {
                             setQuantity((prevQty) =>
@@ -164,7 +164,7 @@ const Product = ({data}) => {
 
                       <div>
                         <button
-                          style={{border: 'none', background: 'none'}}
+                          style={{ border: 'none', background: 'none' }}
                           className="mr-4"
                           onClick={() => {
                             setQuantity((prevQty) =>
@@ -209,7 +209,7 @@ const Product = ({data}) => {
                   <hr className="w-100" />
                   <h4
                     className="mt-5 title font-weight-bolder"
-                    style={{color: '#282828'}}
+                    style={{ color: '#282828' }}
                   >
                     Check Delivery
                   </h4>
@@ -235,9 +235,8 @@ const Product = ({data}) => {
                     </div>
                     <div className="mt-1">
                       <p
-                        className={`error-message ${
-                          deliverable ? 'success' : 'error'
-                        }`}
+                        className={`error-message ${deliverable ? 'success' : 'error'
+                          }`}
                       >
                         {errorMessage}
                       </p>
@@ -253,16 +252,15 @@ const Product = ({data}) => {
                   <div className="col-lg-10 " id="product-tabs">
                     <ul
                       className="nav nav-tabs mt-4 d-flex justify-content-start justify-content-lg-around justify-content-md-around w-100"
-                      style={{background: 'black'}}
+                      style={{ background: 'black' }}
                       id="myTab"
                       role="tablist"
                     >
                       {ProductConstants.tabs.map((tab, index) => (
                         <li className="nav-item" key={index}>
                           <a
-                            className={`nav-link ${
-                              index === activeTab ? 'active' : ''
-                            }`}
+                            className={`nav-link ${index === activeTab ? 'active' : ''
+                              }`}
                             id={`${tab.id}-tab`}
                             data-toggle="tab"
                             href={`#${tab.href}`}
@@ -298,7 +296,7 @@ const Product = ({data}) => {
                               <button
                                 onClick={() => toggleDropdownCustom(index)}
                                 className="w-100 text-left p-3 mt-4"
-                                style={{border: '1px solid transparent'}}
+                                style={{ border: '1px solid transparent' }}
                               >
                                 <span className="ques-product">Question</span>
                                 <span
@@ -310,7 +308,7 @@ const Product = ({data}) => {
                                 </span>
                               </button>
                               {openIndex === index && (
-                                <div style={{fontSize: '14px'}}>
+                                <div style={{ fontSize: '14px' }}>
                                   <br />
                                   <br />
                                   <span className="answer-product">Answer</span>
@@ -342,7 +340,7 @@ const Product = ({data}) => {
                     className="col-lg-2 flex-lg-column mt-0 mt-lg-5 mt-md-5 mt-sm-0 d-flex d-lg-none "
                   >
                     <h4 className="d-flex justify-content-center font-weight-bolder ">
-                      <em style={{fontSize: '2rem'}} className="text-center">
+                      <em style={{ fontSize: '2rem' }} className="text-center">
                         FEATURED PRODUCTS
                       </em>
                     </h4>
@@ -357,7 +355,7 @@ const Product = ({data}) => {
                             >
                               <div
                                 className="w-100 card-container"
-                                style={{maxWidth: '250px', margin: '0 auto'}}
+                                style={{ maxWidth: '250px', margin: '0 auto' }}
                               >
                                 <Image
                                   data={product.variants.nodes[0].image}
@@ -383,18 +381,19 @@ const Product = ({data}) => {
                                     right: '7px',
                                   }}
                                 >
-                                  <p style={{fontWeight: '700'}}>
-                                    {Math.ceil(
-                                      ((product.variants.nodes[0].compareAtPrice
-                                        .amount -
-                                        product.variants.nodes[0].price
-                                          .amount) /
-                                        product.variants.nodes[0].compareAtPrice
-                                          .amount) *
-                                        100,
+                                  <p style={{ fontWeight: '700' }}>
+                                    {product.variants.nodes[0].compareAtPrice ? (
+                                      `${Math.ceil(
+                                        ((product.variants.nodes[0].compareAtPrice.amount -
+                                          product.variants.nodes[0].price.amount) /
+                                          product.variants.nodes[0].compareAtPrice.amount) *
+                                        100
+                                      )}%`
+                                    ) : (
+                                      'No off'
                                     )}
-                                    %
                                   </p>
+
                                 </div>
                               </div>
                             </Link>
@@ -438,7 +437,7 @@ const Product = ({data}) => {
                     className="col-lg-2 flex-lg-column mt-5 mt-lg-4 mt-md-4 mt-sm-2"
                   >
                     <h4 className="d-flex justify-content-center align-items-center font-weight-bolder  text-center">
-                      <em style={{fontSize: '2rem'}}>RELATED PRODUCTS</em>
+                      <em style={{ fontSize: '2rem' }}>RELATED PRODUCTS</em>
                     </h4>
                     <div className="custom-fl-product d-md-flex flex-lg-column ">
                       {TrendingProductsCollection.collection.products.nodes.map(
@@ -452,7 +451,7 @@ const Product = ({data}) => {
                             >
                               <div
                                 className="w-100 card-container"
-                                style={{maxWidth: '250px', margin: '0 auto'}}
+                                style={{ maxWidth: '250px', margin: '0 auto' }}
                               >
                                 <Image
                                   data={product.variants.nodes[0].image}
@@ -478,25 +477,26 @@ const Product = ({data}) => {
                                     right: '5px',
                                   }}
                                 >
-                                  <p style={{fontWeight: '700'}}>
-                                    {Math.ceil(
-                                      ((product.variants.nodes[0].compareAtPrice
-                                        .amount -
-                                        product.variants.nodes[0].price
-                                          .amount) /
-                                        product.variants.nodes[0].compareAtPrice
-                                          .amount) *
-                                        100,
+                                  <p style={{ fontWeight: '700' }}>
+                                    {product.variants.nodes[0].compareAtPrice ? (
+                                      `${Math.ceil(
+                                        ((product.variants.nodes[0].compareAtPrice.amount -
+                                          product.variants.nodes[0].price.amount) /
+                                          product.variants.nodes[0].compareAtPrice.amount) *
+                                        100
+                                      )}%`
+                                    ) : (
+                                      'No off'
                                     )}
-                                    %
                                   </p>
+
                                 </div>
                               </div>
                             </Link>
                             <div className="d-flex justify-content-center align-items-center flex-column">
                               <p
                                 className="font-weight-bold mt-4  text-center text-dark d-flex justify-content-center align-items-center flex-column"
-                                style={{fontSize: '.9rem'}}
+                                style={{ fontSize: '.9rem' }}
                               >
                                 {product.title}
                               </p>
